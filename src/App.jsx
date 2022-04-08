6cb52a04f305d2347d6281d9ce58d9d198af35bd
Gor <gorgan811@gmail.com>
2022-04-08 18:21:26 +0000

    lesson 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import {Messages} from "./Components/Messages";
+
+
+export const App = () => {
+  return <div className="App">
+    <h1>"Messages"</h1>
+    <Messages />
+  </div>
+}
