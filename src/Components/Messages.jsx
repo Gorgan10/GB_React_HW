0c8ae3e56fe66fc51296c061b7b5d1babd9c2839
Gor <gorgan811@gmail.com>
2022-05-03 20:52:52 +0000

    lesson 2

--- src/Components/Messages.jsx
+++ src/Components/Messages.jsx
@@ -1,18 +1,46 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {Input} from "./Input";
 import {Button} from "./Button";
+import {Message} from "./Message";
 
 export const Messages = () => {
+    const [visible, setVisible] = useState(true)
     const [name, setName] = useState('Send')
+
     const [value, setValue] = useState('')
     const [messages, setMessages] = useState([])
-    const [visible, setVisible] = useState(true)
-    const [deleteBtn, setDeleteBtn] = useState(...messages)
 
     const handleClick = () => {
         setMessages([...messages, value])
         setValue('')
     }
+
+
+    useEffect(() => {
+        const lastMessages = messages[messages.length - 1];
+        let timerId = null;
+
+        if (messages.length && lastMessages.author !== "Bot") {
+            timerId = setTimeout(() => {
+                setMessages([
+                    ...messages,
+                    { author: "Bot", message: "hello from bot" },
+                ]);
+            }, 200);
+        }
+
+        return () => clearInterval(timerId);
+    }, [messages]);
+
+    const sendMessage = () => {
+        if (value) {
+            setMessages([
+                ...messages,
+                { author: "User", message: value, date: new Date() },
+            ]);
+            setValue('');
+        }
+    };
 
     const handleChange = (ev) => {
         setValue(ev.target.value)
@@ -26,13 +54,16 @@ export const Messages = () => {
         <button onClick={() => setVisible(!visible)}>
             {visible ? 'hide' : 'show'}
         </button>
-        {visible && <ul>
-            {messages.map(message =>
-                <li>{message}</li>
-            )}
-        </ul>}
+
+        <div className="message">
+            {visible && <ul>
+                {messages.map((message, index) =>
+                    <Message message={message} key={index} />
+                )}
+            </ul>}
+        </div>
         <div className="form">
-            <Input change={handleChange} value={value}/>
+            <Input onClick={sendMessage} change={handleChange} value={value}/>
             <Button name={name} click={handleClick} />
             <button onClick={handleDelete}>Delete</button>
         </div>
